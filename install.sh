c15032d70ecd7af86ec1bdba3bca77fd3ab66694
Ethan Jordan <ejordan418@gmail.com>
2026-03-13 22:18:52 -0500

    chore: removing install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-version=1.9.0
-
-os=$(uname -s | tr '[:upper:]' '[:lower:]')
-mach=$(uname -m)
-plugin="terraform-provider-godaddy"
-
-echo "Warning: installation script no longer supported"
